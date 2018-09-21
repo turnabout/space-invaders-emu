b5d6f4c7646f4c9dc202185b2ff52e1b2e4014b9
Kevin Pageau <pageau.kevin@gmail.com>
2018-09-21 10:10:54 -0400

    Add "Get_Reg_Address" test

--- unit_tests/State.cpp
+++ unit_tests/State.cpp
@@ -36,3 +36,21 @@ TEST(State, PSW_Update_Zero_Bit)
 	PSW_Update_Zero_Bit(0);
 	EXPECT_EQ(state->psw.z, 1);
 }
+
+TEST(State, Get_Reg_Address)
+{
+	state->a = 0x0a;
+	state->b = 0x0b;
+	state->d = 0x0d;
+	state->l = 0x0f;
+
+	uint8_t *regA = Get_Reg_Address(REG_A);
+	uint8_t *regB = Get_Reg_Address(REG_B);
+	uint8_t *regD = Get_Reg_Address(REG_D);
+	uint8_t *regL = Get_Reg_Address(REG_L);
+
+	EXPECT_EQ(*regA, 0x0a);
+	EXPECT_EQ(*regB, 0x0b);
+	EXPECT_EQ(*regD, 0x0d);
+	EXPECT_EQ(*regL, 0x0f);
+}
