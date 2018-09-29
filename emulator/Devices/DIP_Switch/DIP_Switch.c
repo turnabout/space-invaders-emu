85fc7011c21fd48bbda6b1860a7f79aaabed1a47
Kevin Pageau <pageau.kevin@gmail.com>
2018-09-29 16:34:49 -0400

    Fix DIP switch Get/Set functions

--- emulator/Devices/DIP_Switch/DIP_Switch.c
+++ emulator/Devices/DIP_Switch/DIP_Switch.c
@@ -4,10 +4,12 @@ static DIP_Switch dip = {0, 0, 0, 0, 1, 0, 0, 0};
 
 void Set_Switch_Val(uint8_t n, uint8_t val)
 {
-	*((uint8_t *)&dip + n) = val;
+	uint8_t *nP = ((uint8_t *)&dip);
+
+	*nP ^= ((~val + 1) ^ *nP) & (1 << n);
 }
 
 uint8_t Get_Switch_Val(uint8_t n)
 {
-	return *((uint8_t *)&dip + n);
+	return ((1 << n) & *((uint8_t *)&dip)) >> n;
 }
